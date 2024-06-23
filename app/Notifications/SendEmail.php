2a59b0035b7b00dee46bddb1ac2f055c59286f71
C. Tinkelrot <chavit@skyvar.co.il>
2024-06-23 12:44:22 +0300

    try push

--- app/Notifications/SendEmail.php
+++ app/Notifications/SendEmail.php
@@ -2,18 +2,12 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
 class SendEmail extends Notification
 {
-    use Queueable;
-
-    public function __construct(MailMessage $mailMessage)
-    {
-        $this->mailMessage = $mailMessage;
-    }
+    public function __construct(public MailMessage $message) {}
 
     public function via(object $notifiable): array
     {
@@ -22,6 +16,6 @@ class SendEmail extends Notification
 
     public function toMail(object $notifiable): MailMessage
     {
-        return $this->mailMessage;
+        return $this->message;
     }
 }
--- config/mail.php
+++ config/mail.php
@@ -58,8 +58,8 @@ return [
     |
     */
 
-    // 'from' => [
-    //     'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-    //     'name' => env('MAIL_FROM_NAME', 'Example'),
-    // ],
+    'from' => [
+        'address' => env('MAIL_FROM_ADDRESS'),
+        'name' => env('MAIL_FROM_NAME'),
+    ],
 ];
